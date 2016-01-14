ed35fe9d57ad93dd55bff5cea290661dc7961c17
Dzmitry Bahachuk <mayrog87@gmail.com>
2016-01-14 18:44:58 +0300

    qweq

--- app/components/list/list-controller.js
+++ app/components/list/list-controller.js
@@ -2,26 +2,6 @@ export default function ListDirectiveController(Model, $firebaseArray, $firebase
   let firebaseRef =  new Firebase("https://cyber-pedia.firebaseio.com");
   this.listOfArticles = $firebaseArray(firebaseRef);
 
-  // var refImg = new Firebase("https://cyber-pedia.firebaseio.com/images/");
-  // var ImgObj = $firebaseObject(refImg);
-  //
-  // function saveimage(e1) {
-  //    var filename = e1.target.files[0];
-  //    var fr = new FileReader();
-  //    fr.onload = function (res) {
-  //        this.image = res.target.result;
-  //        ImgObj.image = res.target.result;
-  //        ImgObj.$save();
-  //    };
-  //
-  //    fr.readAsDataURL(filename);
-  //     console.log(filename);
-  //  }
-  //
-  // document.getElementById("image-upload").addEventListener('change', saveimage, false);
-
-  // console.log(Img);
-
   this.addNewArticle = function() {
       let ar = {
         name: this.newName,
